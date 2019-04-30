1235cc808a76fed2881c56d3e99011240e714aed
javiercarpio57 <javierrr.carpio57@gmail.com>
2019-04-30 14:08:12 -0600

    queries

--- DB.sql
+++ DB.sql
@@ -1,62 +1,74 @@
-CREATE TABLE clientes
-(
-	 id 			int PRIMARY KEY,
-	 nombre 		varchar,
-	 nit			varchar
-);
+	DROP TABLE IF EXISTS clientes;
+	CREATE TABLE clientes
+	(
+		 id 			SERIAL PRIMARY KEY,
+		 nombre 		varchar,
+		 nit			varchar
+	);
+
+	DROP TABLE IF EXISTS categorias;
+	CREATE TABLE categorias
+	(
+		id 				SERIAL PRIMARY KEY,
+		descripcion 	varchar
+	);
+
+	DROP TABLE IF EXISTS marcas;
+	CREATE TABLE marcas
+	(
+		id 				SERIAL PRIMARY KEY,
+		fabricante 		varchar
+	);
+
+	CREATE TABLE productos
+	(
+		id 				SERIAL PRIMARY KEY,
+		nombre 			varchar,
+		precio 			real,
+		idCategorias 	int,
+		idMarcas 		int,
+		FOREIGN KEY (idCategorias) REFERENCES categorias (id),
+		FOREIGN KEY (idMarcas) REFERENCES marcas (id)
+	);
 
 
-CREATE TABLE productos
-(
-	id 				int PRIMARY KEY,
-	nombre 			varchar,
-	precio 			real,
-	idCategorias 	int,
-	idMarcas 		int
-);
+	CREATE TABLE custom
+	(
+		idProducto 		int PRIMARY KEY,
+		data 			JSON,
+		FOREIGN KEY (idProducto) REFERENCES productos (id)
+	);
+
+	CREATE TABLE facturas
+	(
+		id 				SERIAL PRIMARY KEY,
+		clienteId 		int,
+		fecha 			date,
+		total 			real,
+		FOREIGN KEY (clienteId) REFERENCES clientes (id)
+	);
+
+	CREATE TABLE líneas_de_facturas
+	(
+		facturaId 		SERIAL,
+		idProducto 		int,
+		cantidad 		int,
+		FOREIGN KEY (facturaId) REFERENCES facturas (id),
+		FOREIGN KEY (idProducto) REFERENCES productos (id)
+	);
+
+	insert into custom values 
+	(1,
+		'{
+	  "Talla": 5,
+	  "var2": "Hola2",
+	  "var3": "Hola3"
+		}'
+	);
+
+	select data->>'Talla' from custom
 
 
-CREATE TABLE custom
-(
-	idProducto 		int PRIMARY KEY,
-	data 			JSON
-);
-
-insert into custom values 
-(1,
-	'{
-  "Talla": 5,
-  "var2": "Hola2",
-  "var3": "Hola3"
-	}'
-);
-
-select data->>'Talla' from custom
 
 
-CREATE TABLE categorías
-(
-	id 				int PRIMARY KEY,
-	descripcion 	varchar
-);
 
-CREATE TABLE marcas
-(
- 	id 				int PRIMARY KEY,
- 	fabricante 		varchar
-);
-
-CREATE TABLE facturas
-(
- 	id 				int PRIMARY KEY,
- 	clienteId 		int,
- 	fecha 			date,
- 	total 			real
-);
-
-CREATE TABLE líneas_de_facturas
-(
- 	facturaId 		int,
- 	idProducto 		int,
- 	cantidad 		int
-);
