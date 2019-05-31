17e7c57951f0a5585175472e288aea3cf22d95e8
javiercarpio57 <javierrr.carpio57@gmail.com>
2019-05-31 12:26:13 -0600

    Agregando clientes

--- ElectronApp/src/js/ventas.js
+++ ElectronApp/src/js/ventas.js
@@ -1,5 +1,6 @@
 
 var Pool = require('pg').Pool;
+var fs = require('fs');
 
 var tiendas = ["Tienda A", "Tienda B", "Tienda C"];
 var meses = ['Jan', 'Feb', 'Mar', 'Apr', 'May', "Jun", 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -31,33 +32,31 @@ function getRandom(min, max){
     return Math.floor(random * (Math.abs(max - min) + 1)) + min;
 }
 
+function readFile(file){
+    var options = {
+        encoding: 'utf-8',
+        flag: 'r'
+    }
+
+    var buffer = fs.readFileSync(file, options);
+    console.log("File content: " + buffer);
+}
+
 async function crearFacturas() {
+    var facturasCreadas = [];
     var fechaInicial = document.getElementById("inicio").value;
     var fechaFinal = document.getElementById("final").value;
     var lineas = parseInt(document.getElementById("cantidad").value);
-    var response = await Pool.query('SELECT MAX(id) FROM facturas');
-    // console.log(fechaInicial + " " + fechaFinal + " " + lineas);
 
     if(fechaInicial != '' && fechaFinal != '' && !isNaN(lineas)){
         try {
             for(var i = 0; i < lineas; i++){
-                var response = await Pool.query('SELECT MAX(id), MIN(id) FROM clientes');
-                var maximoCliente = response.rows[0].max;
-                var minimoCliente = response.rows[0].min;
     
                 var response = await Pool.query('SELECT MAX(id), MIN(id) FROM productos');
                 var maximoProducto = response.rows[0].max;
                 var minimoProducto = response.rows[0].min;
-    
-                if(maximoCliente === null){
-                    maximoCliente = 1;
-                    minimoCliente = 1;
-                }
-    
-                if(maximoProducto === null){
-                    maximoProducto = 1;
-                    minimoProducto = 1;
-                }
+
+                //-----------------------------------------------------------------
     
                 var mes = String(fechaInicial).substring(0, 3);
                 var diaI = parseInt(String(fechaInicial).substring(4, 6));
@@ -70,43 +69,72 @@ async function crearFacturas() {
                 var anoF = parseInt(String(fechaFinal).substring(8, 12));
     
                 var mesF = meses.indexOf(mes) + 1;
+
+                //-----------------------------------------------------------------
     
                 var nuevaFactura = getRandom(1, 2);
-                var response = await Pool.query('SELECT MAX(id) FROM facturas');
     
-                if(nuevaFactura == 1 || response.rows[0].max == null){
-                    var idCliente = getRandom(minimoCliente, maximoCliente);
+                if(nuevaFactura == 1 || facturasCreadas.length == 0){
+                    //-----------------------------------------------------------------
+
+                    var nuevoCliente = getRandom(1, 2);
+                    if(nuevoCliente == 1){
+                        var idCliente = getRandom(minimoCliente, maximoCliente);
+                    }else{
+                        //Tomar de la informacion del txt de clientes y hacer el insert
+                    }
+
+                    var response = await Pool.query('SELECT MAX(id), MIN(id) FROM clientes');
+                    var maximoCliente = response.rows[0].max;
+                    var minimoCliente = response.rows[0].min;
+
+                    if(maximoCliente === null){
+                        maximoCliente = 1;
+                        minimoCliente = 1;
+                    }
+        
+                    if(maximoProducto === null){
+                        maximoProducto = 1;
+                        minimoProducto = 1;
+                    }
+
+                    //-----------------------------------------------------------------
+                    
                     var ano = getRandom(anoI, anoF);
                     var mes = getRandom(mesI, mesF);
                     var dia = getRandom(diaI, diaF);
+                    
+                    //-----------------------------------------------------------------
     
-                    // console.log(ano + " " + mes + " " + dia)
                     var tipoTienda = tiendas[getRandom(0, tiendas.length - 1)];
-                    // console.log(tipoTienda);
+
+                    //-----------------------------------------------------------------
+
                     var query = "INSERT INTO facturas(clienteId, fecha, total, tienda) VALUES(" + idCliente + ", '" + ano + "-" + mes + "-" + dia + "', NULL, '" + tipoTienda +"');";
                     console.log("Factura: " + query);
-                    var response = await Pool.query(query);
+                    await Pool.query(query);
+
+                    //-----------------------------------------------------------------
+                    
+                    var response = await Pool.query('SELECT MAX(id) FROM facturas');
+                    var ultimaFactura = response.rows[0].max;
+                    facturasCreadas.push(ultimaFactura);
                 }
     
+                //-----------------------------------------------------------------
                 
-                var response = await Pool.query('SELECT MAX(id), MIN(id) FROM facturas');
-                var maximoFactura = response.rows[0].max;
-                var minimoFactura = response.rows[0].min;
-    
-                if(maximoFactura === null){
-                    maximoFactura = 1;
-                    minimoFactura = 1;
-                }
+                var idFactura = facturasCreadas[Math.floor(Math.random * facturasCreadas.length)];
                 
-                var idFactura = getRandom(minimoFactura, maximoFactura);
-                // console.log(minimoFactura + " " + maximoFactura + " " + idFactura);
-                // console.log(parseInt(minimoProducto) + " " + parseInt(maximoProducto));
+                //-----------------------------------------------------------------
+
                 var idProducto = getRandom(parseInt(minimoProducto), parseInt(maximoProducto));
                 var cantidad = getRandom(1, 10);
+
+                //-----------------------------------------------------------------
     
                 var query = "SELECT checkId(" + idFactura + ", " + idProducto + ", " + cantidad + ");";
                 console.log("Linea de factura: " + query);
-                var response = await Pool.query(query);
+                await Pool.query(query);
             }
         } catch(e){
             console.error("My error", e);
@@ -115,7 +143,7 @@ async function crearFacturas() {
         M.toast({html: '¡Facturas creadas!', classes: 'rounded'});
 
         var query = 'DELETE FROM facturas WHERE total IS NULL;';
-        var response = await Pool.query(query);
+        await Pool.query(query);
     }else{
         M.toast({html: '¡Hubo algun error!', classes: 'rounded'});
     }
